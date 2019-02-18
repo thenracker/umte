a353ee692ff591c2e5579a6972e73d1390f31511
Petr Weissar <weissar.petr@gmail.com>
2019-02-18 14:01:24 +0100

    TODO

--- app/src/main/java/cz/uhk/umteapp/MainActivity.kt
+++ app/src/main/java/cz/uhk/umteapp/MainActivity.kt
@@ -10,3 +10,28 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
     }
 }
+
+/*
+OBSAH
+
+11.2.	JAVA. Seznámení s Android Studiem. Struktura projektu. Malé demo.
+18.2.	Activity. Intent. Listener. Override. Toast. AlertDialog.
+25.2.	UI. View. ViewGroup. Layouts. Intent, ActivityResult
+4.3.	Intent. ActivityResult. RecyclerView.
+11.3.	Opakování RecyclerView
+18.3.	SharedPreferences. Fragment. DialogInterface.
+25.3.	AsyncTask. REST API (https://stag-demo.uhk.cz/ws/web/)
+1.4.	SQLite. Google Map
+8.4.	GPS. Senzors.
+15.4.	Service. Notification.
+22.4.	Velikonoční pondělí
+29.4.	3rd party libraries
+6.5.	Opakování RecyclerView
+
+TERMÍNY
+
+18.3.	Odevzdání zadání projektu
+8.4.	Odevzdání UI / Wireframe
+Zkouškové období	Prezentace projektu proběhne během zkouškového období
+
+ */
